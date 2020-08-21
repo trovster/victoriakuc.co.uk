c10e9599cbfc4ee064f98f445d5c75c524d30f02
Trevor Morris <code@trovster.com>
2020-08-21 14:21:20 +0100

    fix: site config and config baseUrl

--- src/_filters/absoluteUrl.js
+++ src/_filters/absoluteUrl.js
@@ -2,7 +2,7 @@ const { URL } = require('url')
 
 module.exports = (url, base) => {
   try {
-    return new URL(url, base).toString()
+    return new URL(url.replace(/^\//, ''), base).toString()
   } catch (e) {
     return url
   }
