7c2dd3c61c44e6d298e86599d47683ceeefd3263
Trevor Morris <code@trovster.com>
2020-08-21 10:16:15 +0100

    chore: lint js

--- src/js/site.js
+++ src/js/site.js
@@ -1,22 +1,24 @@
-var footer = document.querySelector('footer > p'),
-  addEvent = function (el, type, handler) {
-    if (el.attachEvent) {
-      el.attachEvent('on' + type, handler);
+const footer = document.querySelector('footer > p')
+
+const addEvent = function (el, type, handler) {
+  if (el.attachEvent) {
+    el.attachEvent('on' + type, handler)
+  } else {
+    el.addEventListener(type, handler)
+  }
+}
+
+const scrollToTop = function (scrollDuration) {
+  const scrollStep = -window.scrollY / (scrollDuration / 15)
+  const scrollInterval = window.setInterval(function () {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep)
     } else {
-      el.addEventListener(type, handler);
+      window.clearInterval(scrollInterval)
     }
-  },
-  scrollToTop = function (scrollDuration) {
-    var scrollStep = -window.scrollY / (scrollDuration / 15),
-      scrollInterval = window.setInterval(function () {
-      if (window.scrollY !== 0) {
-        window.scrollBy(0, scrollStep);
-      } else {
-        window.clearInterval(scrollInterval);
-      }
-    }, 15);
-  };
+  }, 15)
+}
 
 addEvent(footer, 'click', function () {
-  scrollToTop(500);
-});
+  scrollToTop(500)
+})
